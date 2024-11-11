a738b8a7e97535401154dd5df0883515cf34f72d
BenithaSri <156029071+BenithaSri@users.noreply.github.com>
2024-11-11 09:12:36 -0600

    Initial Commit

---
+++ ChildMonitoringApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChildMonitoringApp
+//
+//  Created by Benitha Sri Panchagiri on 11/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
